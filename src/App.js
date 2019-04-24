d93378ce907f583addb17132bdfd2d7d2a62d0b2
No Sleep <nosleep@example.com>
2019-04-23 22:19:17 -0400

    fb change

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ class App extends Component {
             target="_blank"
             rel="noopener noreferrer"
           >
-        	daba keri boto kun kun!
+        	I was changed on the feature branch!
               </a>
         </header>
       </div>
